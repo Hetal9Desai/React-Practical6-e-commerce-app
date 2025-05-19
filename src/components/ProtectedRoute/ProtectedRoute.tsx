a2e00549253c72136bb9785fe77a846762b997b4
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-19 17:47:06 +0530

    Fix: Fix the bug related code

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { useAuth } from '../AuthContext';
+import { useAuth } from '../auth/AuthContext';
 
 interface ProtectedRouteProps {
   reverse?: boolean;
