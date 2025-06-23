48debdee0ab1b15bcec557cbb34487659662d2a4
Hetal Desai <hetal.desai@simformsolutions.com>
2025-06-23 18:16:19 +0530

    Work on: Work on suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './components/auth/AuthContext';
 import { Layout } from './components/Layout/Layout';
 import { Signup } from './components/SignUp/SignUp';
 import { Signin } from './components/SignIn/SignIn';
+import { NotFound } from './components/NotFound/NotFound';
 import { ProductsPage } from './components/Product/ProductsPage';
 import { ProfilePage } from './components/Header/ProfilePage';
 import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute';
@@ -34,7 +35,7 @@ const App: React.FC = () => (
             <Route index element={<ProfilePage />} />
           </Route>
 
-          <Route path="*" element={<Navigate to="/signin" replace />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
